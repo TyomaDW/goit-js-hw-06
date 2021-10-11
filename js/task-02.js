e90fc9d4c5e59bffb6d9f5080992dd6d33edf255
Tyoma <risendrums777@gmail.com>
2021-10-12 01:06:34 +0300

    Done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const makeIngredientListItem = (ingredients = []) => {
+  const listRef = document.querySelector('#ingredients');
+  ingredients.forEach(ingredien => {
+    const itemRef = document.createElement('li');
+    itemRef.textContent = ingredien;
+    itemRef.classList.add('item');
+    listRef.append(itemRef);
+  });
+};
+makeIngredientListItem(ingredients);
