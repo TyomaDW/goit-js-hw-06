4b0501fd458dddcb6585c337f382c5a431619a73
Tyoma <risendrums777@gmail.com>
2021-10-12 00:56:13 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,21 +1,11 @@
 const categoriesRef = document.querySelector('#categories');
-const classesRef = categoriesRef.children;
-console.log(`Number of categories: ${classesRef.length}`);
-
-const animalsRef = document.querySelector('.item');
-console.log(`Category: ${animalsRef.firstElementChild.textContent}`);
 console.log(
-  `Elements: ${animalsRef.firstElementChild.nextElementSibling.children.length}`,
+  'Number of categories:',
+  categoriesRef.querySelectorAll('.item').length,
 );
 
-const productsRef = animalsRef.nextElementSibling;
-console.log(`Category: ${productsRef.firstElementChild.textContent}`);
-console.log(
-  `Elements: ${productsRef.firstElementChild.nextElementSibling.children.length}`,
-);
-
-const technologiesRef = productsRef.nextElementSibling;
-console.log(`Category: ${technologiesRef.firstElementChild.textContent}`);
-console.log(
-  `Elements: ${technologiesRef.firstElementChild.nextElementSibling.children.length}`,
-);
+const itemRef = document.querySelectorAll('.item');
+itemRef.forEach(item => {
+  console.log('Category:', item.firstElementChild.textContent);
+  console.log('Elements:', item.lastElementChild.children.length);
+});
