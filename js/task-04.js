e0ce99af8e15ac82a8f8e322a9012b9bc41da732
Tyoma <risendrums777@gmail.com>
2021-10-14 01:23:32 +0300

    Done

---
+++ js/task-04.js
@@ -0,0 +1,16 @@
+let counterValue = 0;
+
+const valueEl = document.querySelector('#value');
+
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+
+decrementBtn.addEventListener('click', event => {
+  counterValue -= 1;
+  valueEl.textContent = counterValue;
+});
+
+incrementBtn.addEventListener('click', event => {
+  counterValue += 1;
+  valueEl.textContent = counterValue;
+});
