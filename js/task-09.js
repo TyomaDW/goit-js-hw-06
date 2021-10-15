ad1998a884c08c5f25fa90e3565e624e13a7a6af
Tyoma <risendrums777@gmail.com>
2021-10-16 00:31:27 +0300

    Done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  changeColor: document.querySelector('.change-color'),
+  color: document.querySelector('.color'),
+};
+
+const onChangeColor = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  refs.color.textContent = `${getRandomHexColor()}`;
+
+  console.log(getRandomHexColor());
+};
+
+refs.changeColor.addEventListener('click', onChangeColor);
