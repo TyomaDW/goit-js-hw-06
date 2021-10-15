604ba2f6efff8e5ce4a459215d1ac582931c839c
Tyoma <risendrums777@gmail.com>
2021-10-16 00:21:01 +0300

    Done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const refs = {
+  form: document.querySelector('.login-form'),
+  email: document.querySelector('input[type="email"]'),
+  password: document.querySelector('input[type="password"]'),
+};
 
+const onSendForm = event => {
+  event.preventDefault();
+
+  const formEl = event.currentTarget.elements;
+  const email = formEl.email.value;
+  const password = formEl.password.value;
+
+  const formData = {
+    email,
+    password,
+  };
+
+  if (!password && !email) {
+    alert('Заполните нужные поля!');
+  } else console.log(formData);
+  refs.form.reset();
+};
+
+refs.form.addEventListener('submit', onSendForm);
