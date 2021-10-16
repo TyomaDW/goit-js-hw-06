6c8d8ef56098f63a40cd98a50c00fc1c2e55dadf
Tyoma <risendrums777@gmail.com>
2021-10-16 15:25:52 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,13 @@
 const refs = { input: document.querySelector('#validation-input') };
 
+const valueLength = parseInt(refs.input.dataset.length);
+
 const onInputInBlur = event => {
-  event.currentTarget.value.length === 6
-    ? (refs.input.className = 'valid')
-    : (refs.input.className = 'invalid');
+  event.currentTarget.value.length !== valueLength
+    ? event.currentTarget.classList.add('invalid') &
+      event.currentTarget.classList.remove('valid')
+    : event.currentTarget.classList.add('valid') &
+      event.currentTarget.classList.remove('invalid');
 };
 
 refs.input.addEventListener('blur', onInputInBlur);
